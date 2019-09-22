b14f009f24a705c50204939a41e75b6419c3591d
ISPOL <insignificantpointoflight@gmail.com>
2019-09-21 23:00:22 -0400

    fixed earlier assumption, displaying pairs now

--- source.js
+++ source.js
@@ -1,12 +1,25 @@
 window.onload = () => {
-    var imageWrapper = document.querySelector(".image-output");
-    var descriptionWrapper = document.querySelector(".description-output");
-    
-    var generate = () => {
-        var randomNumber = Math.floor(Math.random() * data.length);
+    var pickList = [...data]; //valid picks, initialize as full copy of data
+    var isLeft = true; //start at left
+    var pickOne = () => { //pick a picture+text
+        let randomNumber = Math.floor(Math.random()*pickList.length);
+        let imageWrapper = document.querySelector(`.image-output.${isLeft ? "left" : "right"}`);
+        let descriptionWrapper = document.querySelector(`.description-output.${isLeft ? "left" : "right"}`);
         imageWrapper.innerHTML = `<img src="./images/${data[randomNumber].path}"></img>`;
-        randomNumber = Math.floor(Math.random() * data.length);
         descriptionWrapper.innerHTML = `<p>${data[randomNumber].description}</p>`;
+        
+        //reset globals
+        isLeft = !isLeft;
+        pickList.splice(randomNumber,1);
+        if(pickList.length<1){
+            console.log("Presented every image! Repopulating the list...")
+            pickList = [...data];
+        }
+    }
+
+    var generate = () => {
+        pickOne();
+        pickOne();
     }
         
     var button = document.querySelector(".generate");
