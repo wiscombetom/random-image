84c4e3bbcf578fc1efd983e3b097fa554f032f11
wiscombetom <wiscombetom@gmail.com>
2019-09-22 13:19:33 -0400

    Changed the JSON to include ids, started working on the constraints.

--- data.js
+++ data.js
@@ -1,22 +1,37 @@
-var data = [
-        {
-           "path" : "bcb.jpg",
-           "description" : "Bixby Creek Bridge" 
-        },
-        {
-           "path" : "cwb.jpg",
-           "description" : "Calm Waters Bridge" 
-        },
-        {
-           "path" : "ggb.jpg",
-           "description" : "Golden Gate Bridge" 
-        },
-        {
-           "path" : "rb.jpg",
-           "description" : "Regentville Bridge" 
-        },
-        {
-           "path" : "scb.jpg",
-           "description" : "Sea Cliff Bridge" 
-        }
-    ];
+var data = [{
+        "id": 0,
+        "path": "bcb.jpg",
+        "description": "Bixby Creek Bridge",
+        "pairs": [0, 1, 2, 3, 4, 5]
+    },
+    {
+        "id": 1,
+        "path": "cwb.jpg",
+        "description": "Calm Waters Bridge",
+        "pairs": [0, 1, 2, 3, 4, 5]
+    },
+    {
+        "id": 2,
+        "path": "ggb.jpg",
+        "description": "Golden Gate Bridge",
+        "pairs": [0, 1, 2, 3, 4, 5]
+    },
+    {
+        "id": 3,
+        "path": "rb.jpg",
+        "description": "Regentville Bridge",
+        "pairs": [0, 1, 2, 3, 4, 5]
+    },
+    {
+        "id": 4,
+        "path": "scb.jpg",
+        "description": "Sea Cliff Bridge",
+        "pairs": [0, 1, 2, 3, 4, 5]
+    },
+    {
+        "id": 5,
+        "path": "tb.jpg",
+        "description": "Train Bridge",
+        "pairs": [0, 1, 2, 3, 4, 5]
+    }
+];
