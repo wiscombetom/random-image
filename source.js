62171095d49bae2e6bdf4ace31a78b1e4493ae48
ISPOL <insignificantpointoflight@gmail.com>
2019-09-22 12:28:11 -0400

    keyframe animation tinkering

--- source.js
+++ source.js
@@ -9,6 +9,20 @@ window.onload = () => {
         imageWrapper.innerHTML = `<img src="./images/${pickList[randomNumber].path}"></img>`;
         descriptionWrapper.innerHTML = `<p>${pickList[randomNumber].description}</p>`;
         
+        //animation
+        
+ 
+        imageWrapper.classList.remove(`${leftOrRight}Slide`)
+        descriptionWrapper.classList.remove(`${leftOrRight}Slide`)
+
+        setTimeout(()=>{
+            imageWrapper.offsetWidth = imageWrapper.offsetWidth;
+            descriptionWrapper.offsetWidth = descriptionWrapper.offsetWidth;
+
+        },10);
+        imageWrapper.classList.add(`${leftOrRight}Slide`)
+        descriptionWrapper.classList.add(`${leftOrRight}Slide`)
+
         //reset globals
         isLeft = !isLeft;
         pickList.splice(randomNumber,1);
