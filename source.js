31753124c196a1d032bc14620a601a63b0b1c355
ISPOL <insignificantpointoflight@gmail.com>
2019-09-26 13:31:51 -0400

    animation timing

--- source.js
+++ source.js
@@ -1,20 +1,22 @@
 window.onload = () => {
     var pickList = [...data]; //valid picks, initialize as full copy of data
-    var isLeft = true; //start at left
+    //var isLeft = true; //start at left
 
-    var leftWrapper = document.querySelector('left');
-    var rightWrapper = document.querySelector('right');
+    //var leftWrapper = document.querySelector('left');
+    //var rightWrapper = document.querySelector('right');
 
     var picks = {"left":null,"right":null}
     var tagRestrictions = [];
+    var timer = 0;
+    //var leftOrRight = () => {return isLeft ? "left" : "right";}
+    var animationTypes = ["SlideIn","SlideOut"];
 
-    var pick = () =>{
+    var generate = () =>{
         if(pickList.length === 0){
             pickList = [...data];
         }
         let randomNumber = Math.floor(Math.random() * pickList.length);
-        let leftOrRight = isLeft ? "left" : "right";
-        console.log(pickList);
+
         //pick left, no tag restrictions
         picks.left = pickList[randomNumber];
         pickList.splice(randomNumber,1)
@@ -32,7 +34,6 @@ window.onload = () => {
                 pickCounter--;
             }
         }
-        console.log("rightpicklist", rightPickList);
         //if no valid picks left?
         if(rightPickList.length===0){
             /*start over?? (risks infinite loop is dataset is bad)
@@ -53,25 +54,23 @@ window.onload = () => {
         tagRestrictions = [];
 
         //TODO
-  //      AnimationEffect()
         setImage(picks.left.id,"left");
         setImage(picks.right.id,"right");
         setTitle(picks.left.id,"left");
         setTitle(picks.right.id,"right");
         setDescription(picks.left.id,"left");
         setDescription(picks.right.id,"right");
-  //      AnimationEffect()
 
         picks = {"left":null,"right":null};
     }
     /*
     var pickOne = () => { //pick a picture+text
         let randomNumber = Math.floor(Math.random() * pickList.length);
-        let leftOrRight = isLeft ? "left" : "right";
+        let leftOrRight() = isLeft ? "left" : "right";
         
         
-        let imageWrapper = document.querySelector(`.image-${leftOrRight}`);
-        let descriptionWrapper = document.querySelector(`.description-${leftOrRight}`);
+        let imageWrapper = document.querySelector(`.image-${leftOrRight()}`);
+        let descriptionWrapper = document.querySelector(`.description-${leftOrRight()}`);
         imageWrapper.innerHTML = `<img src="./images/${pickList[randomNumber].path}"></img>`;
         descriptionWrapper.innerHTML = `<p>${pickList[randomNumber].description}</p>`;
         
@@ -79,14 +78,14 @@ window.onload = () => {
         //animation
 
 
-        imageWrapper.classList.remove(`${leftOrRight}Slide`)
-        descriptionWrapper.classList.remove(`${leftOrRight}Slide`)
+        imageWrapper.classList.remove(`${leftOrRight()}Slide`)
+        descriptionWrapper.classList.remove(`${leftOrRight()}Slide`)
 
         setTimeout(() => {
             imageWrapper.offsetWidth = imageWrapper.offsetWidth;
             descriptionWrapper.offsetWidth = descriptionWrapper.offsetWidth;
-            imageWrapper.classList.add(`${leftOrRight}Slide`);
-            descriptionWrapper.classList.add(`${leftOrRight}Slide`);
+            imageWrapper.classList.add(`${leftOrRight()}Slide`);
+            descriptionWrapper.classList.add(`${leftOrRight()}Slide`);
 
         }, 10);
 
@@ -109,32 +108,69 @@ window.onload = () => {
         //find the element
         let imageWrapper = document.querySelector(`.image-${side}`);
 
-        //replace the contents with the image
-        imageWrapper.innerHTML = `<img src="./images/${data[index].path}"></img>`;
+        //slideOut animation
+        if(timer>10)setAnimation(imageWrapper, animationTypes[1], side);
+
+        setTimeout(()=>{
+            //replace the contents with the image
+            imageWrapper.innerHTML = `<img src="./images/${data[index].path}"></img>`;
+    
+            //slideIn animation
+            setAnimation(imageWrapper, animationTypes[0], side);
+        },timer);
+
     }
 
     var setTitle = (index, side) => {
         //find the element
         let titleWrapper = document.querySelector(`.title-${side}`);
 
-        //replace the contents with the description
-        titleWrapper.innerHTML = `<p>${data[index].title}</p>`;
+        //slideOut animation
+        if(timer>10)setAnimation(titleWrapper, animationTypes[1], side);
+
+        
+        setTimeout(()=>{
+            //replace the contents with the description
+            titleWrapper.innerHTML = `<p>${data[index].title}</p>`;
+
+            //slideIn animation
+            setAnimation(titleWrapper, animationTypes[0], side);
+        },timer);
+
     }
 
     var setDescription = (index, side) => {
         //find the element
         let descriptionWrapper = document.querySelector(`.description-${side}`);
 
-        //replace the contents with the description
-        descriptionWrapper.innerHTML = `<p>${data[index].description}</p>`;
+
+        //slideOut animation
+        if(timer>10)setAnimation(descriptionWrapper, animationTypes[1], side);
+
+        setTimeout(()=>{
+            //replace the contents with the description
+            descriptionWrapper.innerHTML = `<p>${data[index].description}</p>`;
+            
+            //slideIn animation
+            setAnimation(descriptionWrapper, animationTypes[0], side);
+            
+        },timer);
     }
 
-    var generate = () => {
-        pick();
+    var setAnimation = (element, animation, side)=>{
+        //remove all animation classes
+        animationTypes.forEach((name)=>{
+            element.classList.remove(`${side+name}`);
+        });
+        setTimeout(() => {
+            element.offsetWidth = element.offsetWidth;
+            element.classList.add(`${side+animation}`);
+        }, 0);
+        
     }
-
     var button = document.querySelector(".generate");
     button.addEventListener(`click`, generate);
 
     generate();
+    timer = 1800;
 }
