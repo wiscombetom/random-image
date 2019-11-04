9e413223c636eb8de93e003584b63dcb85aaf62e
wiscombetom <wiscombetom@gmail.com>
2019-11-03 23:18:34 -0500

    images and data
    added more sample images from unsample.net and added sample data

--- data.js
+++ data.js
@@ -39,5 +39,138 @@ var data = [{
         "title": "Train Bridge",
         "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
         "tags": ["Lorem", "Amet"]
+    },
+    {
+        "id": 6,
+        "path": "sp0.jpg",
+        "title": "Sample 0",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Lorem", "Amet"]
+    },
+    {
+        "id": 7,
+        "path": "sp1.jpg",
+        "title": "Sample 1",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Dolor", "Sit"]
+    },
+    {
+        "id": 8,
+        "path": "sp2.jpg",
+        "title": "Sample 2",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Lorem", "Amet"]
+    },
+    {
+        "id": 9,
+        "path": "sp3.jpg",
+        "title": "Sample 3",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Dolor", "Sit"]
+    },
+    {
+        "id": 10,
+        "path": "sp4.jpg",
+        "title": "Sample 4",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Lorem", "Amet"]
+    },
+    {
+        "id": 11,
+        "path": "sp5.jpg",
+        "title": "Sample 5",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Lorem", "Amet"]
+    },
+    {
+        "id": 12,
+        "path": "sp6.jpg",
+        "title": "Sample 6",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Lorem", "Amet"]
+    },
+    {
+        "id": 13,
+        "path": "sp7.jpg",
+        "title": "Sample 7",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Dolor", "Sit"]
+    },
+    {
+        "id": 14,
+        "path": "sp8.jpg",
+        "title": "Sample 8",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Dolor", "Sit"]
+    },
+    {
+        "id": 15,
+        "path": "sp9.jpg",
+        "title": "Sample 9",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Lorem", "Amet"]
+    },
+    {
+        "id": 16,
+        "path": "sp10.jpg",
+        "title": "Sample 10",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Lorem", "Amet"]
+    },
+    {
+        "id": 17,
+        "path": "sp11.jpg",
+        "title": "Sample 11",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Lorem", "Amet"]
+    },
+    {
+        "id": 18,
+        "path": "sp12.jpg",
+        "title": "Sample 12",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Dolor", "Sit"]
+    },
+    {
+        "id": 19,
+        "path": "sp13.jpg",
+        "title": "Sample 13",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Dolor", "Sit"]
+    },
+    {
+        "id": 20,
+        "path": "sp14.jpg",
+        "title": "Sample 14",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Lorem", "Amet"]
+    },
+    {
+        "id": 21,
+        "path": "sp15.jpg",
+        "title": "Sample 15",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Lorem", "Amet"]
+    },
+    {
+        "id": 22,
+        "path": "sp16.jpg",
+        "title": "Sample 16",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Lorem", "Amet"]
+    },
+    {
+        "id": 23,
+        "path": "sp17.jpg",
+        "title": "Sample 17",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Lorem", "Amet"]
+    },
+    {
+        "id": 24,
+        "path": "sp18.jpg",
+        "title": "Sample 18",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe commodi tempora nam delectus. Tenetur officia iure voluptas odio! Vero, eveniet.",
+        "tags": ["Lorem", "Amet"]
     }
 ];
